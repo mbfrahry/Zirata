772b4c68189aa8f177ee52765a96e4c596fc42f1
wacko7178 <mbfrahry@yahoo.com>
2015-09-11 22:54:55 -0600

    Extending block bank

--- app/src/main/java/th/zirata/BuildScreen.java
+++ app/src/main/java/th/zirata/BuildScreen.java
@@ -39,6 +39,7 @@ public class BuildScreen extends GLScreen{
 	public static final int BLOCK_BANK_ENERGY = 2;
 	public static final int BLOCK_BANK_MULTIPLIER = 3;
 
+	boolean draggingBankBlock;
 
 	public BuildScreen(Game game) {
         super(game);
@@ -64,124 +65,130 @@ public class BuildScreen extends GLScreen{
 		showBlockBank = false;
 		blockBankOption = BLOCK_BANK_TURRET;
 		ownedBlocksByType = getBlocksFromType(TurretBlock.class);
+		draggingBankBlock = false;
     }
 
 	@Override
 	public void update(float deltaTime) {
 		List<TouchEvent> touchEvents = game.getInput().getTouchEvents();
 		int len = touchEvents.size();
-		for(int i = 0; i < len; i++){
-			TouchEvent event = touchEvents.get(i);
-			touchPoint.set(event.x, event.y);
-			guiCam.touchToWorld(touchPoint);
-			
-			if(event.type == TouchEvent.TOUCH_UP){
-				if(selectedActiveBlock != null && selectedBankBlock != null){
-					PlayerSave.bankedBlocks.remove(selectedBankBlock);
-					PlayerSave.activeBlocks.remove(selectedActiveBlock);
-					selectedBankBlock.position.x = selectedActiveBlock.position.x;
-					selectedBankBlock.position.y = selectedActiveBlock.position.y;
-					PlayerSave.bankedBlocks.add(selectedActiveBlock);
-					PlayerSave.activeBlocks.add(selectedBankBlock);
-					resetBlockBank();
-					if(selectedBankBlock.getClass() == TurretBlock.class){
-						game.setScreen(new BlockDirectionScreen(game, selectedBankBlock.position, PlayerSave.activeBlocks.indexOf(selectedBankBlock)));
-					}
-					else {
-						selectedActiveBlock = null;
+		if(len > 0){
+			for(int i = 0; i < len; i++) {
+				TouchEvent event = touchEvents.get(i);
+				touchPoint.set(event.x, event.y);
+				guiCam.touchToWorld(touchPoint);
+
+				if (event.type == TouchEvent.TOUCH_UP) {
+
+
+					if (selectedBankBlock != null && selectedActiveBlock == null) {
 						selectedBankBlock = null;
-					}
-
-					return;
-				}
-
-				if(selectedBankBlock != null && selectedActiveBlock == null){
-					selectedBankBlock = null;
-					showBlockBank = true;
-				}
-
-				if(!showBlockBank) {
-					if (OverlapTester.pointInRectangle(backBounds, touchPoint)) {
-						game.setScreen(new MainMenuScreen(game));
-						return;
-					}
-
-					if (OverlapTester.pointInRectangle(forwardBounds, touchPoint)) {
-						Settings.save(game.getFileIO());
-						PlayerSave.save(game.getFileIO());
-						game.setScreen(new GameScreen(game));
-						return;
-					}
-
-					if (OverlapTester.pointInRectangle(blockMenuBounds, touchPoint)) {
 						showBlockBank = true;
-						return;
 					}
 
-					Rectangle pBlockBounds;
-					for (int j = 0; j < potentialBlocks.size(); j++) {
-						Block currBlock = potentialBlocks.get(j);
-						pBlockBounds = new Rectangle(currBlock.position.x - 12, currBlock.position.y - 12, 25, 25);
-						if (OverlapTester.pointInRectangle(pBlockBounds, touchPoint)) {
-							if (Settings.spaceBucks >= Settings.nextBlockCost) {
-								PlayerSave.activeBlocks.add(currBlock);
-								PlayerSave.save(game.getFileIO());
-								potentialBlocks.remove(j);
-								getPotentialBlocks();
-								Settings.spaceBucks -= Settings.nextBlockCost;
-								Settings.nextBlockCost = PlayerSave.activeBlocks.size();
-								Settings.save(game.getFileIO());
+					if (!showBlockBank) {
+						if (OverlapTester.pointInRectangle(backBounds, touchPoint)) {
+							game.setScreen(new MainMenuScreen(game));
+							return;
+						}
+
+						if (OverlapTester.pointInRectangle(forwardBounds, touchPoint)) {
+							Settings.save(game.getFileIO());
+							PlayerSave.save(game.getFileIO());
+							game.setScreen(new GameScreen(game));
+							return;
+						}
+
+						if (OverlapTester.pointInRectangle(blockMenuBounds, touchPoint)) {
+							showBlockBank = true;
+							return;
+						}
+
+						Rectangle pBlockBounds;
+						for (int j = 0; j < potentialBlocks.size(); j++) {
+							Block currBlock = potentialBlocks.get(j);
+							pBlockBounds = new Rectangle(currBlock.position.x - 12, currBlock.position.y - 12, 25, 25);
+							if (OverlapTester.pointInRectangle(pBlockBounds, touchPoint)) {
+								if (Settings.spaceBucks >= Settings.nextBlockCost) {
+									PlayerSave.activeBlocks.add(currBlock);
+									PlayerSave.save(game.getFileIO());
+									potentialBlocks.remove(j);
+									getPotentialBlocks();
+									Settings.spaceBucks -= Settings.nextBlockCost;
+									Settings.nextBlockCost = PlayerSave.activeBlocks.size();
+									Settings.save(game.getFileIO());
+								}
+								return;
 							}
-							return;
+						}
+						/*
+						for (int j = 0; j < PlayerSave.activeBlocks.size(); j++) {
+							Block currBlock = PlayerSave.activeBlocks.get(j);
+							pBlockBounds = new Rectangle(currBlock.position.x - 12, currBlock.position.y - 12, 25, 25);
+							if (OverlapTester.pointInRectangle(pBlockBounds, touchPoint)) {
+								game.setScreen(new BlockUpgradeScreen(game, j));
+								return;
+							}
+						}*/
+					} else {
+						checkBankBlocks(touchPoint);
+					}
+				}
+
+				if (event.type == TouchEvent.TOUCH_DOWN) {
+					if (showBlockBank == true) {
+						Rectangle bankBlockBounds;
+						for (int j = 0; j < ownedBlocksByType.size(); j++) {
+							Block currBlock = ownedBlocksByType.get(j);
+							bankBlockBounds = new Rectangle(currBlock.position.x - 12, currBlock.position.y - 12, 25, 25);
+							if (OverlapTester.pointInRectangle(bankBlockBounds, touchPoint)) {
+								selectedBankBlock = currBlock;
+							}
+						}
+
+					}
+
+				}
+
+				if (event.type == TouchEvent.TOUCH_DRAGGED) {
+					if (selectedBankBlock != null) {
+						if (showBlockBank == true) {
+							showBlockBank = false;
+						}
+						Rectangle pBlockBounds;
+						for (int j = 0; j < PlayerSave.activeBlocks.size(); j++) {
+							Block currBlock = PlayerSave.activeBlocks.get(j);
+							pBlockBounds = new Rectangle(currBlock.position.x - 12, currBlock.position.y - 12, 25, 25);
+							if (OverlapTester.pointInRectangle(pBlockBounds, touchPoint)) {
+								selectedActiveBlock = currBlock;
+								return;
+							}
+							draggingBankBlock = true;
 						}
 					}
-/*
-					for (int j = 0; j < PlayerSave.activeBlocks.size(); j++) {
-						Block currBlock = PlayerSave.activeBlocks.get(j);
-						pBlockBounds = new Rectangle(currBlock.position.x - 12, currBlock.position.y - 12, 25, 25);
-						if (OverlapTester.pointInRectangle(pBlockBounds, touchPoint)) {
-							game.setScreen(new BlockUpgradeScreen(game, j));
-							return;
-						}
-					}*/
-				}
-				else{
-					checkBankBlocks(touchPoint);
 				}
 			}
 
-			if(event.type == TouchEvent.TOUCH_DOWN){
-				if(showBlockBank == true){
-					Rectangle bankBlockBounds;
-					for(int j = 0; j < ownedBlocksByType.size(); j++) {
-						Block currBlock = ownedBlocksByType.get(j);
-						bankBlockBounds = new Rectangle(currBlock.position.x - 12, currBlock.position.y - 12, 25, 25);
-						if (OverlapTester.pointInRectangle(bankBlockBounds, touchPoint)) {
-							selectedBankBlock = currBlock;
-						}
-					}
-
+		}else if (draggingBankBlock){
+			draggingBankBlock = false;
+			if(selectedActiveBlock != null && selectedBankBlock != null){
+				PlayerSave.bankedBlocks.remove(selectedBankBlock);
+				PlayerSave.activeBlocks.remove(selectedActiveBlock);
+				selectedBankBlock.position.x = selectedActiveBlock.position.x;
+				selectedBankBlock.position.y = selectedActiveBlock.position.y;
+				PlayerSave.bankedBlocks.add(selectedActiveBlock);
+				PlayerSave.activeBlocks.add(selectedBankBlock);
+				resetBlockBank();
+				if(selectedBankBlock.getClass() == TurretBlock.class){
+					game.setScreen(new BlockDirectionScreen(game, selectedBankBlock.position, PlayerSave.activeBlocks.indexOf(selectedBankBlock)));
+				}
+				else {
+					selectedActiveBlock = null;
+					selectedBankBlock = null;
 				}
 
+				return;
 			}
-
-			if(event.type == TouchEvent.TOUCH_DRAGGED){
-				if(selectedBankBlock != null){
-					if(showBlockBank == true ) {
-						showBlockBank = false;
-					}
-					Rectangle pBlockBounds;
-					for (int j = 0; j < PlayerSave.activeBlocks.size(); j++) {
-						Block currBlock = PlayerSave.activeBlocks.get(j);
-						pBlockBounds = new Rectangle(currBlock.position.x - 12, currBlock.position.y - 12, 25, 25);
-						if (OverlapTester.pointInRectangle(pBlockBounds, touchPoint)) {
-							selectedActiveBlock = currBlock;
-							return;
-						}
-					}
-				}
-			}
-
 		}
 	}
 
